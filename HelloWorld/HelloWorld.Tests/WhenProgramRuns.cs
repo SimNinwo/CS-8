b0f43fb2b66a8f0490ff0541d23bf82f39cb543d
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2020-08-25 10:54:23 +0100

    chapter 1 done

---
+++ HelloWorld/HelloWorld.Tests/WhenProgramRuns.cs
@@ -0,0 +1,31 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace HelloWorld.Tests
+{
+    [TestClass]
+    public class WhenProgramRuns
+    {
+        private string _consoleOutput;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var w = new StringWriter();
+
+            Console.SetOut(w);
+
+            Program.Main(new string[0]);
+
+            _consoleOutput = w.GetStringBuilder().ToString().Trim();
+        }
+
+        [TestMethod]
+        public void SaysHelloWorld()
+        {
+            Assert.AreEqual("Hello World!", _consoleOutput);
+        }
+    }
+}
--- ReadME.md
+++ ReadME.md
@@ -1,7 +1,3 @@
 # Learning C# language using 'Programming C# 8.0'
 
-i.  Solutions listed in chapters.
-
-ii. Branches created based on Chapters.
-
-- Simeon
+- HelloWorld with testProject
